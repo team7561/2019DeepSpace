49a3d12dbc4e1f8dd821ca26420349a2f6b887b5
VaSIMR200 <44719194+VaSIMR200@users.noreply.github.com>
2019-10-07 12:40:59 +1100

    Working drive to point with reversing if more efficient, and using offset target

--- 2019DeepSpace/src/main/java/frc/robot/autonomous/state/DriveToPoint.java
+++ 2019DeepSpace/src/main/java/frc/robot/autonomous/state/DriveToPoint.java
@@ -16,47 +16,54 @@ public class DriveToPoint implements frc.robot.autonomous.state.State {
         this.bearing = bearing;
     }
     public boolean run(Robot robot) {
+        final double currentHeading = robot.viveMeasurements.get_Y_rot();
         final Coordinate currentLocation = robot.viveMeasurements.getLocation();
-        final double distance = Coordinate.getDistance(currentLocation, destination);
-
-        double offsetDistance = 0.3 * distance;
-        if (distance < 0.5)
-            offsetDistance = 0;
-        //offsetDistance = distance;
-
-        Coordinate targetLocation = Coordinate.getApproachCoordinate(destination,  bearing,  offsetDistance);
-        SmartDashboard.putNumber("Target Location X", targetLocation.getX());
-        SmartDashboard.putNumber("Target Location Z", targetLocation.getZ());
-        SmartDashboard.putNumber("Target Location Heading", bearing);
-
         if(!robot.viveMeasurements.isValidCooardinates(currentLocation)) {
             robot.drivetrain.drive(0, 0);
             return false;
         }
-        double speed = Constants.AUTO_DRIVE_SPEED;
-        double slow_speed= Constants.AUTO_DRIVE_SLOW_SPEED;
+
+        final double distance = Coordinate.getDistance(currentLocation, destination);
         if (distance < Constants.DISTANCE_TOLERANCE) {
             robot.drivetrain.drive(0, 0);
             return true;
         }
-        final double destinationHeading = Coordinate.getHeading(currentLocation, targetLocation);
 
-        final double currentHeading = robot.viveMeasurements.get_Y_rot();
-        double headingError = (currentHeading - destinationHeading + 900) % 360 - 180;
-        SmartDashboard.putNumber("DriveToPoint Distance", distance);
-        SmartDashboard.putNumber("DriveToPoint destinationHeading", destinationHeading);
-        SmartDashboard.putNumber("DriveToPoint currentHeading", currentHeading);
-        SmartDashboard.putNumber("DriveToPoint headingError", headingError);
+        double offsetDistance = 0.3 * distance;
+        if (distance < 0.5)
+            offsetDistance = 0;
+
+        Coordinate targetLocation = Coordinate.getApproachCoordinate(destination,  bearing,  offsetDistance);
+        double targetHeading = Coordinate.getHeading(currentLocation, targetLocation);
+        double headingError = (currentHeading - targetHeading + 900) % 360 - 180;
+
+        Coordinate alternateTargetLocation = Coordinate.getApproachCoordinate(destination,  bearing,  -offsetDistance);
+        final double alternateTargetHeading = Coordinate.getHeading(currentLocation, alternateTargetLocation);
+        double alternateHeadingError = (currentHeading - alternateTargetHeading + 900) % 360 - 180;
+        // 180 - alternateHeadingError
+        alternateHeadingError = (180 - alternateHeadingError + 900) % 360 - 180;
 
         boolean reverse = false;
-       /*if ( headingError > 90 || headingError < -90) {
-            //System.out.println("Reversing");
+        System.out.println("Heading errors " + headingError + ", " + alternateHeadingError);
+        if (Math.abs(alternateHeadingError) < Math.abs(headingError)) {
             reverse = true;
-            // 180 - headingError
-            headingError = (180 - headingError + 900) % 360 - 180;
+            System.out.println("Reversing ");
+            targetLocation = alternateTargetLocation;
+            targetHeading = alternateTargetHeading;
+            headingError = alternateHeadingError;
+        }
 
-            // or should it be 180 + headingError?
-        }*/
+        SmartDashboard.putNumber("Target Location X", targetLocation.getX());
+        SmartDashboard.putNumber("Target Location Z", targetLocation.getZ());
+        SmartDashboard.putNumber("Target Location Heading", targetHeading);
+
+        double speed = Constants.AUTO_DRIVE_SPEED;
+        double slow_speed= Constants.AUTO_DRIVE_SLOW_SPEED;
+
+        SmartDashboard.putNumber("DriveToPoint Distance", distance);
+        SmartDashboard.putNumber("DriveToPoint destinationHeading", targetHeading);
+        SmartDashboard.putNumber("DriveToPoint currentHeading", currentHeading);
+        SmartDashboard.putNumber("DriveToPoint headingError", headingError);
 
         final double headingDivisor = 40;
         double left = 0.5 - headingError / headingDivisor;
